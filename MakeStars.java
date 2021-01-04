26503587deb1e69990d1ccbd0e5b038c4f786027
Igbathscaller <Igbathscaller@gmail.com>
2021-01-04 13:09:27 -0500

    this might work

--- MakeStars.java
+++ MakeStars.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class MakeStars{
     public static void main(String[]args){
-
+        Scanner stuff = new Scanner(System.in);
+        Scanner line;
+        while (stuff.hasNextLine()){
+            line = new Scanner(stuff.nextLine());
+            while(line.hasNext()){
+                System.out.print(stars(line.next()));
+            }
+        }
+        stuff.close();
     }
     
+    private static String stars(String item){
+        String a = "";
+        for(int i = 0; i<item.length(); i++){
+            a+='*';
+        }
+        return a;
+    } 
 }
